9a1107c737aef0b6dc6374a08514648bc258ebc7
robcxyz <rob@sudoblock.io>
2022-11-15 00:35:51 +0530

    chore: fix test deps

--- tests/tackle/test_main.py
+++ tests/tackle/test_main.py
@@ -11,9 +11,3 @@ def test_main_deploy(change_base_dir):
     output = tackle('deploy', chain='eth', network='mainnet', contract='bmc')
 
     assert output['script'] == 'eth/scripts/deploy.sh'
-
-
-def test_main_build2(change_base_dir):
-    output = tackle(override={'a_input': 'foo'})
-
-    assert output['a_input'] == 'foo'
